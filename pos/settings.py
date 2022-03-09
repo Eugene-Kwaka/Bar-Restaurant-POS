f386a520ce1b2fc13593cb8197e307145f4a3130
Eugene Kirima <kwakaeugene@gmail.com>
2022-03-09 13:02:30 +0300

    still deploying

--- pos/settings.py
+++ pos/settings.py
@@ -51,13 +51,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    
+    ]
 
 ROOT_URLCONF = 'pos.urls'
 
@@ -128,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # location of your application, should not be public web accessible
 STATICFILES_DIRS = [
