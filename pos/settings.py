fef1528b28755b18de42680380df773a411cc677
Eugene Kirima <kwakaeugene@gmail.com>
2022-03-09 16:08:52 +0300

    changed topnavigation

--- pos/settings.py
+++ pos/settings.py
@@ -66,9 +66,7 @@ ROOT_URLCONF = 'pos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'pos/posApp/templates/posApp')
-            ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
